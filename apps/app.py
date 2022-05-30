47b29b6811f820d11782512660429acc7417f232
hinatha <hinatha0110@gmail.com>
2022-05-30 13:43:29 +0900

    Finished crud operation

--- apps/app.py
+++ apps/app.py
@@ -3,8 +3,10 @@ from pathlib import Path
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
+csrf = CSRFProtect()
 
 # create_app関数を作成する
 def create_app():
@@ -16,13 +18,15 @@ def create_app():
         SQLALCHEMY_DATABASE_URI=
             f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        SQLALCHRMY_ECHO=True
+        SQLALCHRMY_ECHO=True,
+        WTF_CSRF_SECRET_KEY = "AuwzyszU5sugKN7KZs6f",
     )
     # SQLAlchemyとAppの連携
     db.init_app(app)
 
     # MigrateとAppの連携
     Migrate(app, db)
+    csrf.init_app(app)
 
     # crudパッケージからviewsをimportする
     from apps.crud import views as crud_views
