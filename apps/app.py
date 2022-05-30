ceb258a9b9f1c48fea69c74bb5582b4787a0fd5e
hinatha <hinatha0110@gmail.com>
2022-05-30 13:49:46 +0900

    Set config

--- apps/app.py
+++ apps/app.py
@@ -5,22 +5,17 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
+from apps.config import config
+
 db = SQLAlchemy()
 csrf = CSRFProtect()
 
 # create_app関数を作成する
-def create_app():
+def create_app(config_key):
     # Flaskインスタンス生成
     app = Flask(__name__)
-    # Appのconfig設定
-    app.config.from_mapping(
-        SECRET_KEY="dw4lfOk490387dEYW5",
-        SQLALCHEMY_DATABASE_URI=
-            f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        SQLALCHRMY_ECHO=True,
-        WTF_CSRF_SECRET_KEY = "AuwzyszU5sugKN7KZs6f",
-    )
+    app.config.from_object(config[config_key])
+    
     # SQLAlchemyとAppの連携
     db.init_app(app)
 
