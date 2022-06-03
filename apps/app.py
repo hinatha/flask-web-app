293fd6662e8747c1667d345f9657ee71c7e688e9
hinatha <hinatha0110@gmail.com>
2022-06-03 13:10:42 +0900

    Finished login

--- apps/app.py
+++ apps/app.py
@@ -1,7 +1,6 @@
-from pathlib import Path
-
 from flask import Flask
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
@@ -10,12 +9,20 @@ from apps.config import config
 db = SQLAlchemy()
 csrf = CSRFProtect()
 
+# LoginManagerをインスタンス化する
+login_manager = LoginManager()
+# login_view属性に未ログイン時にリダイレクトするエンドポイントを指定する
+login_manager.login_view = "auth.signup"
+# login_message属性にログイン後に表示するメッセージを指定する
+# ここでは何も表示しないよう空を指定する
+login_manager.login_message = ""
+
 # create_app関数を作成する
 def create_app(config_key):
     # Flaskインスタンス生成
     app = Flask(__name__)
     app.config.from_object(config[config_key])
-    
+
     # SQLAlchemyとAppの連携
     db.init_app(app)
 
@@ -23,10 +30,19 @@ def create_app(config_key):
     Migrate(app, db)
     csrf.init_app(app)
 
+    # login_managerをアプリケーションと連携する
+    login_manager.init_app(app)
+
     # crudパッケージからviewsをimportする
     from apps.crud import views as crud_views
 
     # register_blueprintを使いviewsのcrudをアプリへ登録する
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+    # これから作成するauthパッケージからviewsをimportする
+    from apps.auth import views as auth_views
+
+    # register_blueprintを使いviewsのauthをアプリへ登録する
+    app.register_blueprint(auth_views.auth, url_prefix="/auth")
 
     return app
