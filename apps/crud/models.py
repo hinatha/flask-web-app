a917d8aaa11f9da1cf0feb6d1895f15e03b04274
hinatha <hinatha0110@gmail.com>
2022-06-03 14:53:12 +0900

    Fix gitignore(remove .pt file)

--- apps/crud/models.py
+++ apps/crud/models.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
-from apps.app import db
-from werkzeug.security import generate_password_hash
+from apps.app import db, login_manager
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 # db.Modelを継承したUserクラスを作成する
-class User(db.Model):
+class User(db.Model, UserMixin):
     # テーブル名を指定する
     __tablename__ = "users"
     # カラムを定義する
@@ -15,6 +16,9 @@ class User(db.Model):
     password_hash = db.Column(db.String)
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    # backrefを利用しrelation情報を設定する
+    user_images = db.relationship("UserImage", backref="user")
 
     # パスワードをセットするためのプロパティ
     @property
@@ -26,4 +30,16 @@ class User(db.Model):
     def password(self, password):
         self.password_hash = generate_password_hash(password)
 
+    # パスワードチェックをする
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
+    # メールアドレス重複チェックをする
+    def is_duplicate_email(self):
+        return User.query.filter_by(email=self.email).first() is not None
+
+
+# ログインしているユーザー情報を取得する関数を作成する
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
