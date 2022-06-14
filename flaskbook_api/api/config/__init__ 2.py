0a04873307cdc6cd52ae949bf69c047df174a6ff
hinatha <hinatha0110@gmail.com>
2022-06-14 19:04:06 +0900

    Edit readme

--- flaskbook_api/api/config/__init__ 2.py
+++
@@ -1,7 +0,0 @@
-from flaskbook_api.api.config import base,local
-
-
-config = {
-    "base": base.Config,
-    "local": local.LocalConfig,
-}
--- flaskbook_api/api/preprocess 2.py
+++
@@ -1,8 +0,0 @@
-import torchvision
-
-# 画像データの前処理
-# Pytorchに対応する型に変更するためテンソル型に変換（ndarray型に似ている）
-def image_to_tensor(image):
-    """画像データをテンソル型の数値データへ変換"""
-    image_tensor = torchvision.transforms.functional.to_tensor(image)
-    return image_tensor
